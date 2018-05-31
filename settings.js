30882b17d7113fb88f0bf54f662ff37e2fdd78c3
Nick O'Leary <nick.oleary@gmail.com>
2018-05-31 14:04:45 +0100

    Add req node

--- settings.js
+++ settings.js
@@ -1,4 +1,11 @@
 module.exports = {
     uiPort: process.env.PORT,
-    credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET
+    credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET,
+    adminAuth: {
+        type: "credentials",
+        users: [],
+        default: {
+            permissions: "read"
+        }
+    }
 }
