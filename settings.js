c1b7e1e6a0f20decf369bff8e61d55328b15ce00
Nick O'Leary <nick.oleary@gmail.com>
2018-05-31 14:20:49 +0100

    Disable auth to debu credential handling

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 module.exports = {
     uiPort: process.env.PORT,
     credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET,
-    adminAuth: {
-        type: "credentials",
-        users: [],
-        default: {
-            permissions: "read"
-        }
-    },
+    // adminAuth: {
+    //     type: "credentials",
+    //     users: [],
+    //     default: {
+    //         permissions: "read"
+    //     }
+    // },
     logging: {
         console:{
             level:"trace"
